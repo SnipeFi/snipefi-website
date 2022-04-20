642e7f16094b9d0dac9759bf86d67a2e19ba9d5e
SnipeFi <snipefinance7@gmail.com>
2022-04-20 22:38:17 +0700

    add README.md

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
   return (
-    <div className='bg-blue-500'>
+    <div className='bg-red-500'>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
