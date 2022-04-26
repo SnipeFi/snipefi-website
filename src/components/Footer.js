03c07af9d560692a522e0ea983bbc9f86eb331c5
SnipeFi <snipefinance7@gmail.com>
2022-04-26 23:06:40 +0700

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+export default function Footer() {
+    return (
+        <div className='flex justify-center items-center h-16 bg-black text-white'>
+            <p>Copyright © 2022 SnipeFi All rights reserved.</p>
+        </div>
+    )
+}
